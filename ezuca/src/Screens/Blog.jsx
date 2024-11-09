8c1cbb85ebca53a02927e107129f7fe1d9d27d44
Zeeshan Siddiqui <155655770+Zeeshan-Siddiqui123@users.noreply.github.com>
2024-11-09 12:14:43 +0500

    Update Blog.jsx

--- ezuca/src/Screens/Blog.jsx
+++ ezuca/src/Screens/Blog.jsx
@@ -1,107 +1,103 @@
-import React from 'react'
-import Header from '../Components/Header'
-import { Card2 } from '../Components/Card'
-import { MdLocationPin } from 'react-icons/md'
-import { FaCalendarAlt } from 'react-icons/fa'
-import Footer from '../Components/Footer'
+import React from 'react';
+import Header from '../Components/Header';
+import { MdLocationPin } from 'react-icons/md';
+import { FaCalendarAlt } from 'react-icons/fa';
+import Footer from '../Components/Footer';
 
 const Blog = () => {
   return (
-    <div>
-      <div>
-        <h1 className="text-black flex items-center justify-start text-5xl ml-[95px]">Latest News & Events</h1>
-      </div>
-      <div className="flex items-center justify-evenly flex-row flex-wrap gap-2">
-        <div className="flex items-center justify-around flex-col w-[500px] ">
-          <img src="https://themewagon.github.io/ezuca/images/event-1.jpg" alt="" />
-          <h1 className="text-3xl text-[grey] ml-5">The Complete Financial Analyst Training & Investing Course</h1>
-          <div className='flex items-center justify-between gap-9'>
-            <div className='flex items-center justify-start'>
-              <MdLocationPin color='#19c880' />
-              <h4 className='text-[#80808077]'>40 Baria Sreet 133/2 NewYork City, US</h4>
-            </div>
-            <div className='flex items-center justify-start'>
-              <FaCalendarAlt color='#19c880' />
-              <h4 className='text-[#80808077]'>10 Dec - 12 dec</h4>
-            </div>
-          </div>
-        </div>
-        <div className="flex items-center justify-around flex-col gap-4">
-          <div className='flex items-center justify-around flex-row gap-4'>
-            <div>
-              <img src="https://themewagon.github.io/ezuca/images/event-2.jpg" alt="" />
-            </div>
-            <div className='flex items-start justify-start flex-col'>
-              <div className='flex items-center justify-start gap-2'>
-                <FaCalendarAlt color='#19c880' />
-                <h4 className='text-[#80808077]'>22 Mar 2018</h4>
-              </div>
-              <h1 className="text-xl text-black ">Persolize Online Learning <br />Experience</h1>
-              <span><i className='text-[#19c880]'>Ms. Lara Croft</i> / 02 Comments</span>
-              <span className='text-lg text-[#8080808e]'>Excepteur sint occaecat cupidatat non <br /> proident, sunt in culpa qui officia deserunt.</span>
-            </div>
-          </div>
-          <div className='flex items-center justify-around flex-row gap-4'>
-            <div>
-              <img src="https://themewagon.github.io/ezuca/images/event-3.jpg" alt="" />
-            </div>
-            <div className='flex items-start justify-start flex-col'>
-              <div className='flex items-center justify-start gap-2'>
-                <FaCalendarAlt color='#19c880' />
-                <h4 className='text-[#80808077]'>22 Mar 2018</h4>
-              </div>
-              <h1 className="text-xl text-black ">Which investment project should <br />my company choose?</h1>
-              <span><i className='text-[#19c880]'>Ms. Lara Croft</i> / 02 Comments</span>
-              <span className='text-lg text-[#8080808e]'>Excepteur sint occaecat cupidatat non <br /> proident, sunt in culpa qui officia deserunt.</span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="flex gap-1 w-full flex-row mt-32 flex-wrap items-center justify-center">
-        <div className='flex gap-1 flex-wrap justify-center'>
-          <div className='flex gap-1 flex-col flex-wrap'>
-            <div className='flex gap-1 flex-row flex-wrap'>
-              <img src="https://themewagon.github.io/ezuca/images/a.jpg" alt="" className='w-[171px] h-[171px]' />
-              <img src="https://themewagon.github.io/ezuca/images/b.jpg" alt="" className='w-[171px] h-[171px]' />
-            </div>
-            <div className='flex'>
-              <img src="https://themewagon.github.io/ezuca/images/h.jpg" alt="" className='w-[346px] h-[173.61px]' />
-            </div>
-          </div>
-          <div className='flex gap-1 flex-col flex-wrap'>
+    <div className="p-4 md:p-8">
+      {/* <Header /> */}
 
-            <img src="https://themewagon.github.io/ezuca/images/c.jpg" alt="" className='w-[352px] h-[352px]' />
-            <img src="https://themewagon.github.io/ezuca/images/m.jpg" alt="" className='w-[352px] h-[173.61px]' />
+      {/* Title Section */}
+      <div className="flex items-center justify-start ml-6 md:ml-24 mb-10">
+        <h1 className="text-black text-3xl md:text-5xl font-bold">Latest News & Events</h1>
+      </div>
+
+      {/* Blog Content Section */}
+      <div className="flex flex-col lg:flex-row items-start lg:items-center justify-center gap-6 lg:gap-10 flex-wrap mb-12">
+        {/* Main Event Card */}
+        <div className="flex flex-col items-start w-full lg:w-[500px] gap-4">
+          <img src="https://themewagon.github.io/ezuca/images/event-1.jpg" alt="Event 1" className="w-full rounded-md" />
+          <h1 className="text-2xl md:text-3xl text-gray-700">The Complete Financial Analyst Training & Investing Course</h1>
+          <div className="flex justify-between w-full gap-4 text-gray-500">
+            <div className="flex items-center gap-2">
+              <MdLocationPin color="#19c880" />
+              <h4>40 Baria Street 133/2, New York City, US</h4>
+            </div>
+            <div className="flex items-center gap-2">
+              <FaCalendarAlt color="#19c880" />
+              <h4>10 Dec - 12 Dec</h4>
+            </div>
           </div>
         </div>
-        <div className='flex flex-col gap-1 flex-wrap justify-center'>
-          <div className='flex flew-row gap-1 flex-wrap justify-center'>
-            <img src="https://themewagon.github.io/ezuca/images/d.jpg" alt="" className='w-[171px] h-[171px]' />
-            <img src="https://themewagon.github.io/ezuca/images/e.jpg" alt="" className='w-[171px] h-[171px]' />
-            <img src="https://themewagon.github.io/ezuca/images/g.jpg" alt="" className='w-[291px] h-[171px]' />
+
+        {/* Sidebar Event Cards */}
+        <div className="flex flex-col gap-6 w-full lg:w-auto">
+          {/* Small Event Card 1 */}
+          <div className="flex gap-4 items-start">
+            <img src="https://themewagon.github.io/ezuca/images/event-2.jpg" alt="Event 2" className="w-32 h-32 rounded-md" />
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center gap-2 text-gray-500">
+                <FaCalendarAlt color="#19c880" />
+                <h4>22 Mar 2018</h4>
+              </div>
+              <h1 className="text-lg text-black">Personalize Online Learning Experience</h1>
+              <span className="text-sm text-[#19c880]">Ms. Lara Croft / 02 Comments</span>
+              <span className="text-gray-500 text-sm">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.</span>
+            </div>
           </div>
-          <div className='flex flex-row gap-1 flex-wrap justify-center'>
-            <div className='flex flex-col gap-1 flex-wrap'>
-              <img src="https://themewagon.github.io/ezuca/images/i.jpg" alt="" className='w-[171px] h-[171px]' />
-              <img src="https://themewagon.github.io/ezuca/images/l.jpg" alt="" className='w-[171px] h-[171px]' />
-              {/* <img src="https://themewagon.github.io/ezuca/images/n.jpg" alt="" /> */}
-            </div>
-            <div className='flex flex-col gap-1'>
-              <img src="https://themewagon.github.io/ezuca/images/j.jpg" alt="" className='w-[292px] h-[351px]' />
-            </div>
-            <div className='flex flex-col gap-1 img-flex'>
-              <img src="https://themewagon.github.io/ezuca/images/k.jpg" alt="" className='w-[171px] h-[171px]' />
-              <img src="https://themewagon.github.io/ezuca/images/o.jpg" alt="" className='w-[171px] h-[171px]' />
+          {/* Small Event Card 2 */}
+          <div className="flex gap-4 items-start">
+            <img src="https://themewagon.github.io/ezuca/images/event-3.jpg" alt="Event 3" className="w-32 h-32 rounded-md" />
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center gap-2 text-gray-500">
+                <FaCalendarAlt color="#19c880" />
+                <h4>22 Mar 2018</h4>
+              </div>
+              <h1 className="text-lg text-black">Which investment project should my company choose?</h1>
+              <span className="text-sm text-[#19c880]">Ms. Lara Croft / 02 Comments</span>
+              <span className="text-gray-500 text-sm">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.</span>
             </div>
           </div>
         </div>
       </div>
-      <div className='flex gap-2 items-center justify-center ml-[535px] set-image'>
-        <img src="https://themewagon.github.io/ezuca/images/n.jpg" alt="" />
+
+      {/* Image Grid Section */}
+      <div className="flex flex-wrap gap-2 justify-center mt-12">
+        <div className="flex flex-col gap-2">
+          <div className="flex gap-2">
+            <img src="https://themewagon.github.io/ezuca/images/a.jpg" alt="Image A" className="w-[80px] h-[80px] md:w-[171px] md:h-[171px] object-cover rounded-md" />
+            <img src="https://themewagon.github.io/ezuca/images/b.jpg" alt="Image B" className="w-[80px] h-[80px] md:w-[171px] md:h-[171px] object-cover rounded-md" />
+          </div>
+          <img src="https://themewagon.github.io/ezuca/images/h.jpg" alt="Image H" className="w-full md:w-[346px] md:h-[173.61px] object-cover rounded-md" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <img src="https://themewagon.github.io/ezuca/images/c.jpg" alt="Image C" className="w-full md:w-[352px] md:h-[352px] object-cover rounded-md" />
+          <img src="https://themewagon.github.io/ezuca/images/m.jpg" alt="Image M" className="w-full md:w-[352px] md:h-[173.61px] object-cover rounded-md" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="flex gap-2">
+            <img src="https://themewagon.github.io/ezuca/images/d.jpg" alt="Image D" className="w-[80px] h-[80px] md:w-[171px] md:h-[171px] object-cover rounded-md" />
+            <img src="https://themewagon.github.io/ezuca/images/e.jpg" alt="Image E" className="w-[80px] h-[80px] md:w-[171px] md:h-[171px] object-cover rounded-md" />
+            <img src="https://themewagon.github.io/ezuca/images/g.jpg" alt="Image G" className="w-[80px] h-[80px] md:w-[291px] md:h-[171px] object-cover rounded-md" />
+          </div>
+          <div className="flex gap-2">
+            <img src="https://themewagon.github.io/ezuca/images/i.jpg" alt="Image I" className="w-[80px] h-[80px] md:w-[171px] md:h-[171px] object-cover rounded-md" />
+            <img src="https://themewagon.github.io/ezuca/images/l.jpg" alt="Image L" className="w-[80px] h-[80px] md:w-[171px] md:h-[171px] object-cover rounded-md" />
+            <img src="https://themewagon.github.io/ezuca/images/k.jpg" alt="Image K" className="w-[80px] h-[80px] md:w-[171px] md:h-[171px] object-cover rounded-md" />
+          </div>
+        </div>
       </div>
-      <Footer/>
+
+      {/* Centered Image */}
+      <div className="flex justify-center mt-8">
+        <img src="https://themewagon.github.io/ezuca/images/n.jpg" alt="Image N" className="w-[300px] md:w-[400px] rounded-md" />
+      </div>
+
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
